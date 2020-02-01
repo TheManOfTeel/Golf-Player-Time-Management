e0ef49e03971be1f08d943fedf0a15766ee4fa4c
Danny <teelman1996@gmail.com>
2020-01-31 23:24:24 -0500

    router changed to dashboard in url

--- GPTM Cloud App/admin-app/src/app/auth.guard.ts
+++ GPTM Cloud App/admin-app/src/app/auth.guard.ts
@@ -18,7 +18,7 @@ export class AuthGuard implements CanActivate {
     return new Promise ((resolve, reject) => {
       this.userService.getCurrentUser()
       .then(user => {
-        this.router.navigate(['/user']);
+        this.router.navigate(['/dashboard']);
         return resolve(false);
       }, err => {
         return resolve(true);
