599b683bd68cc00a372c2b5778a7c5f3d68e0cc9
bcarlin1 <43416588+bcarlin1@users.noreply.github.com>
2020-02-02 18:18:12 -0500

    Second attempt at pushing - Code
    Had to split up my original branch commits because I had code mixed in with documents. This contains (from the original pull request): began coding the Golf Course Registration page

---
+++ GPTM Cloud App/admin-app/src/app/golf-course-registration/golf-course-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-golfcourseregistration',
+  templateUrl: './golf-course-registration.component.html',
+  styleUrls: ['./golf-course-registration.component.css']
+})
+export class GolfCourseRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
