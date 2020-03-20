573212c21c07c5faf1d7cf989d1eb636450e7a66
Danny <teelman1996@gmail.com>
2020-03-19 21:15:06 -0400

    correct the queue after the calculation adjustment

--- GPTM Cloud App/admin-app/src/app/player-overview/tables/active-player-table/active-player-table.component.ts
+++ GPTM Cloud App/admin-app/src/app/player-overview/tables/active-player-table/active-player-table.component.ts
@@ -42,7 +42,7 @@ export class ActivePlayerTableComponent implements OnInit {
         i++;
         const queueRef = firebase.database().ref('GolfCourse/' + courseName + '/WaitTimes/Hole' + holeNum);
         queueRef.update({
-          Queue: holeQueue,
+          Queue: holeQueue - 1,
           WaitTime: (holePar * 3.2 * holeQueue).toFixed(0)
         });
       });
