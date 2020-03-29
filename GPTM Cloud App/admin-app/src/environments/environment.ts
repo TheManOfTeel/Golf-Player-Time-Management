35e6c74cf768dc396911bf7554493d1238065ad5
Danny <teelman1996@gmail.com>
2020-03-28 20:26:26 -0400

    production mode enabled, version set to 1

--- GPTM Cloud App/admin-app/src/environments/environment.ts
+++ GPTM Cloud App/admin-app/src/environments/environment.ts
@@ -5,7 +5,7 @@ import * as firebase from 'firebase';
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true
 };
 
 const config = {
