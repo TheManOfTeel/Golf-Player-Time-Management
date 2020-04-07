3225437c3b8bda2f05daa01e63c7397dc80d85ba
Danny <teelman1996@gmail.com>
2020-04-06 20:26:17 -0400

    collapsible sidenav, graph scaling, code cleanup

--- GPTM Cloud App/admin-app/src/app/register/register.component.ts
+++ GPTM Cloud App/admin-app/src/app/register/register.component.ts
@@ -40,28 +40,6 @@ export class RegisterComponent implements OnInit {
   // For the radio buttons
   holes: number[] = [9, 18];
 
-  // For the dropdown list
-  // numbers: NumberOfHoles[] = [
-    // {value: 1, viewValue: 1},
-    // {value: 2, viewValue: 2},
-    // {value: 3, viewValue: 3},
-    // {value: 4, viewValue: 4},
-    // {value: 5, viewValue: 5},
-    // {value: 6, viewValue: 6},
-    // {value: 7, viewValue: 7},
-    // {value: 8, viewValue: 8},
-    // {value: 9, viewValue: 9},
-    // {value: 10, viewValue: 10},
-    // {value: 11, viewValue: 11},
-    // {value: 12, viewValue: 12},
-    // {value: 13, viewValue: 13},
-    // {value: 14, viewValue: 14},
-    // {value: 15, viewValue: 15},
-    // {value: 16, viewValue: 16},
-    // {value: 17, viewValue: 17},
-  //   {value: 18, viewValue: 18},
-  // ];
-
   placeSearch: any;
   autocomplete: any;
   verified: boolean;
